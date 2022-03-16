b35452f4c62aa5c5204726e52518489e8a5f8012
Connor Atkins <connoratkins@hotmail.co.uk>
2022-03-16 12:30:44 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// ABOUT
+// ------------------------------------------------
+const navBar = document.querySelector('#navbar'),
+  aboutTxt = document.querySelector('#about-txt'),
+  aboutSkills = document.querySelector('#about-skills'),
+  aboutBtn = document.querySelector('#about-btn');
+
+// aboutBtn.addEventListener('click', function () {
+//   showHideAbout();
+// });
+
+// showHideAbout();
+
+// function showHideAbout() {
+//   if (navBar.style.height == '6rem') {
+//     navBar.style.height = '60vh';
+//   } else {
+//     navBar.style.height = '6rem';
+//   }
+//   if (aboutTxt.style.transform == 'translateX(-100%)') {
+//     aboutTxt.style.transform = 'translateX(0%)';
+//   } else {
+//     aboutTxt.style.transform = 'translateX(-100%)';
+//   }
+//   if (aboutTxt.style.opacity == '0') {
+//     aboutTxt.style.opacity = '1';
+//   } else {
+//     aboutTxt.style.opacity = '0';
+//   }
+//   if (aboutSkills.style.transform == 'translateY(100%)') {
+//     aboutSkills.style.transform = 'translateY(0%)';
+//   } else {
+//     aboutSkills.style.transform = 'translateY(100%)';
+//   }
+//   if (aboutSkills.style.opacity == '0') {
+//     aboutSkills.style.opacity = '1';
+//   } else {
+//     aboutSkills.style.opacity = '0';
+//   }
+// }
+
+//  CAROUSEL
+//  ----------------------------------------------------------
